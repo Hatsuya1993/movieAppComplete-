62fae21c1796baad8c82533389fb346cd77f49f1
Hazrul <hal1993@hotmail.com>
2022-08-22 15:41:05 +0800

    Update banner

--- client/src/Feature/Banner.tsx
+++ client/src/Feature/Banner.tsx
@@ -8,10 +8,10 @@ const Banner : React.FC = () => {
     const { currentUser } = useAuth()
     return (
         <div className='w-full relative'>
-            <video muted loop autoPlay src={movieVideo} className='h-[300px] w-full flex items-center justify-center'></video>
+            <video muted loop autoPlay src={movieVideo} className='object-fill h-[300px] w-full flex items-center justify-center'></video>
             <div className='w-full'>
-            <div className='absolute top-32 w-80 text-center left-0 right-0 ml-auto mr-auto opacity-50'>
-                <p className='text-white'>Find The Movies You Want</p>
+            <div className='flex flex-col gap-3 absolute top-32 w-full text-center left-0 right-0 ml-auto mr-auto opacity-50'>
+                <p className='text-white text-3xl'>Find The Movies You Want</p>
                 {currentUser ? <ButtonComponent onClick={() => {}}>Explore</ButtonComponent> : <Link to={'/login'}><ButtonComponent onClick={() => {}}>Login</ButtonComponent></Link>}
             </div>
             </div>
