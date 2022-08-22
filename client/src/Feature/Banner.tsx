cdd09885c2439e51c5eebdf8f82940e7b4db61fc
Hazrul <hal1993@hotmail.com>
2022-08-22 16:55:54 +0800

    text size change

--- client/src/Feature/Banner.tsx
+++ client/src/Feature/Banner.tsx
@@ -11,7 +11,7 @@ const Banner : React.FC = () => {
             <video muted loop autoPlay src={movieVideo} className='object-fill h-[300px] md:h-[500px] w-full flex items-center justify-center'></video>
             <div className='w-full'>
             <div className='flex flex-col gap-3 absolute md:top-52 top-32 w-full text-center left-0 right-0 ml-auto mr-auto opacity-50'>
-                <p className='text-white text-3xl'>Find The Movies You Want</p>
+                <p className='text-white text-xl md:text-3xl'>Find The Movies You Want</p>
                 {currentUser ? <ButtonComponent onClick={() => {}}>Explore</ButtonComponent> : <Link to={'/login'}><ButtonComponent onClick={() => {}}>Login</ButtonComponent></Link>}
             </div>
             </div>
