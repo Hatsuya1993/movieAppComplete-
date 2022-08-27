f7acca0cadb74c7251afa481ff1888e37bedfdf3
Hazrul <hal1993@hotmail.com>
2022-08-27 09:08:01 +0800

    Added functionality for detail

--- client/src/Feature/Detail.tsx
+++ client/src/Feature/Detail.tsx
@@ -1,23 +1,25 @@
 import { CircularProgress } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
 import React from 'react'
 import { useLocation } from 'react-router-dom'
-import { backdrop } from '../Redux/initialState'
+import { available_on_type, backdrop } from '../Redux/initialState'
 import { actionType } from '../Redux/reducer'
 import { useStateValue } from '../Redux/StateProvider'
-import { getMoviesVideo } from '../Utils/fetchShows'
+import { getMoviesVideo, getShows } from '../Utils/fetchShows'
 
 const Detail : React.FC = () => {
     const location = useLocation()
     const data : any = location.state
-    const [{loading, get_movies_images}, dispatch] = useStateValue()
+    const [{loading, get_movies_images, available_on}, dispatch] = useStateValue()
     React.useEffect(() => {
         const fetchData = async () => {
-            await getMoviesImages()
+            await getMovies()
         }
         fetchData()
     }, [])
-    const getMoviesImages = async () => {
+    const getMovies = async () => {
         const dataImages = await getMoviesVideo(data.id)
+        const dataAvailable = await getShows(data.original_title)
         dispatch({
             type: actionType.SET_LOADING,
             loading: true
@@ -27,13 +29,17 @@ const Detail : React.FC = () => {
             get_movies_images: dataImages
         })
         dispatch({
+            type: actionType.SET_AVAILABLE_ON,
+            available_on: dataAvailable.available_on
+        })
+        dispatch({
             type: actionType.SET_LOADING,
             loading: false
         })
     }
     
     return (
-        <div className='w-full h-full p-7'>
+        <div className='w-full h-full p-7 flex flex-col gap-5'>
             <div className='bg-slate-100 w-80 mx-auto p-3 rounded-lg'>
             <div className='w-full flex flex-col gap-4'>
                 <div className='w-full'>
@@ -50,8 +56,30 @@ const Detail : React.FC = () => {
                 {loading ? <div className='w-full text-center'>
                     <CircularProgress size='50px' isIndeterminate color='orange.400' /></div>
                 : get_movies_images && get_movies_images.backdrops.length > 0 &&  get_movies_images.backdrops.map((each : backdrop) => (
-                    <img src={`https://image.tmdb.org/t/p/original${each.file_path}`} alt="" />
+                    <img key={each.file_path} src={`https://image.tmdb.org/t/p/original${each.file_path}`} alt="" />
                 ))}
+            </div>
+            <div className='w-80 mx-auto flex flex-col gap-3'>
+                <p className='text-center'>Available On</p>
+                {loading ? <div className='w-full text-center'>
+                    <CircularProgress size='50px' isIndeterminate color='orange.400' /></div> : available_on && available_on.length > 0 ? (
+                        <div className='flex flex-col gap-3'>
+                            {available_on.map((each : available_on_type) => (
+                                <a href={`${each.link}`} target="_blank" rel="noopener noreferrer" key={each.name}>
+                            <motion.div whileTap={{scale:0.9}} className='bg-slate-100 p-2 rounded-lg flex items-center justify-between'>
+                                <div>
+                                <p>{each.name}</p>
+                                <p>Price: {each.price}</p>
+                                </div>
+                                <div>
+                                    <img src={`${each.thumbnail}`} alt="" />
+                                </div>
+                            </motion.div>
+                            </a>
+                        ))}
+                        </div>
+
+                    ) : null}
             </div>
         </div>
     )
